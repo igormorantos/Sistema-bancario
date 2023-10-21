7c73cf7e50658c299e03c83b44a6920f27c9dce3
igor moreira <igormorantosdev@outlook.com>
2023-10-21 20:29:54 -0300

    iniciando a documentação da api

--- src/servidor.js
+++ src/servidor.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const rotas = require('./rotas')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json')
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docks", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(rotas)
 
 module.exports = app;
